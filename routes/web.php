de060a57cfa18d89bb984ea91ebdf44cb7af58fb
pyaesone21 <pyaesone.puppey@gmail.com>
2024-03-07 13:22:29 +0630

    no message

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\category\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
